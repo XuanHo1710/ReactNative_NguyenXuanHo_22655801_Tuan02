edf3e394eb63bc33cc34083a1f06c15c69e9081e
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-27 08:46:24 +0700

    26. Finish done task 2 - lesson 26

--- dist/index.js
+++ dist/index.js
@@ -241,3 +241,9 @@ function downloadFile() {
     });
 }
 downloadFile().then((message) => console.log(message));
+// 26. Use async/await with setTimeout to simulate a 5-second wait.
+async function waitFiveSeconds() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("Waited 5 seconds");
+}
+waitFiveSeconds();
