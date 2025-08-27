c7ec13ef5df4c865e1c97130b252d0e06ba274e4
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-27 07:16:26 +0700

    1. Finish done task 2 - lesson 1

---
+++ baitapreact.js
@@ -0,0 +1,9 @@
+// A. Basics with Promise
+// 1. Create a Promise that returns the string "Hello Async" after 2 seconds.
+const helloAsync = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Hello Async");
+    }, 2000);
+});
+
+helloAsync.then((message) => console.log(message));
