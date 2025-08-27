a56d76a59fdb4b3293917c538d2b9250c64eace4
Xuanho1710 <xuanhodcbas@gmail.com>
2025-08-27 07:38:15 +0700

    6. Finish done task 2 - lesson 6

--- dist/index.js
+++ dist/index.js
@@ -50,3 +50,10 @@ function simulateTask(time) {
     });
 }
 simulateTask(1500).then((message) => console.log(message));
+// 6. Use Promise.all() to run 3 simulated Promises in parallel and print the result.
+const task1 = simulateTask(1000);
+const task2 = simulateTask(2000);
+const task3 = simulateTask(1500);
+Promise.all([task1, task2, task3]).then((results) => {
+    console.log("All tasks completed:", results);
+});
